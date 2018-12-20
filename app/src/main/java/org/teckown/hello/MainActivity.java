ae7c3b163c117a2547adb2e63b6f281827d73d52
jjuyeoni <cwa302@naver.com>
2018-12-20 14:44:33 +0900

    first commit

---
+++ app/src/main/java/org/teckown/hello/MainActivity.java
@@ -0,0 +1,26 @@
+package org.teckown.hello;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.floatingActionButton:
+                intent = new Intent(this, SearchActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+}
